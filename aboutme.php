6c0d3df4ef9c1ff7c52bbd6ef809795ca3666fe2
RVJonah <barker0184@hotmail.com>
2019-09-06 17:18:27 +0100

    CSS overhaul and html semantics changed to be more user friendly

--- aboutme.php
+++ aboutme.php
@@ -3,93 +3,106 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./css/index.css">
-    <link rel="stylesheet" type="text/css" href="./css/aboutme.css">
+    <link rel="stylesheet" type="text/css" href="./css/base.css">
     <title>
         About David Barker
     </title>
 </head>
 <body>
-<div class="title">
-    <h2 class="fulltitle">David Barker</h2>
-</div>
-<div class="flexboxcontainer">
-    <div class="flexbox20 gallery">
-    <p class="frame"><img src="./images/rvc_crest.jpg" alt="Royal Veterinary College Crest"><br>Royal Veterinary College, London, UK</p>
-    <p class="frame"><img src="./images/imperial_crest.png" alt="Imperial College Crest"><br>Imperial College, London, UK</p>
+    <div id="content">
+        <h1 class="fulltitle">David Barker</h1>
     
+    <aside class="flexbox3side">
+        <img src="./images/rvc_crest.jpg" alt="Royal Veterinary College Crest">
+        <figcaption>Royal Veterinary College, London, UK</figcaption>
+        <img src="./images/imperial_crest.png" alt="Imperial College Crest">
+        <figcaption>Imperial College, London, UK</figcaption>
+    </aside>
+        <main class="flexbox3middle">
+            <article>
+                <section>
+                    <div>
+                        <img id="profilePic" src="./images/david.jpg" alt="Picture of David Barker">
+                    <div>
+                    <div>
+                        <h2>What I can offer</h2>     
+                            <ul>
+                                <li>Determination</li>
+                                <li>Drive</li>
+                                <li>Real world experience</li>
+                                <li>Quick to learn</li>
+                                <li>Cool under pressure</li>
+                            </ul>
+                        <h2>What I'm looking for</h2>
+                            <ul>
+                                <li>Further development of my coding skills</li>
+                                <li>The feeling of being valued member of a team</li>
+                                <li>A chance to produce high quality engaging content</li>
+                                <li>A stimulating and challenging experience</li>
+                                <li>Forward thinking employer</li>
+                            </ul>
+                    </div>
+                </section>
+                <section>
+                    <h2>Who am I?</h2>
+                    <p>
+                        Born: South East London in 1984.
+                    </p>
+                    <p>
+                        Early Education: I left school at 16 to attend a WestMinster KingsWay College in London to allow myself the opportunity to take 4 A-Levels. 4 As (The 
+                        highest grades available at the time) in Math. Physics, Chemistry and Biology followed in 2002.
+                    </p>
+                    <p>
+                        First Career: Given my love of animals I decided to persue a career as a vet. I was told at the time that as I went to an inner London college my chances 
+                        of achieving this were low, but never having been one to take being told something can't be done at face value I decided that taking a year out to 
+                        confirm my grades and gain some further experience would lean things in my favour. This proved to be the case and the following year (Sept. 2003) 
+                        I started my studies at the Royal Veterinary College (RVC).
+                    </p>
+                </section>
+                <section>
+                    <h2>University and work until now</h2>
+                    <p>
+                        During my studies at the RVC I spent 12 months at Imperial College on an intercalated BSc course achieving a BSc in Medical Sciences with Molecular
+                        Biology and Cellular Biology with a 2:1 grade with honours. Returning to the RVC I completed my veterinary studies in 2009 and graduated with a 
+                        Bachelor of Veterinary Medicine. I preceeded from there to work as a first opinion veterinarian for 10 years.
+                    </p>
+                    <p>
+                        After 7 years of practice and feeling dissatisfied with my career I under took a post graduate certificate in Small Animal Surgery gaining the certificate in 2016. 
+                        Despite this and many other attempts at increasing my work satisfaction I continued to be unhappy at work and eventually in December of 2018 I decided that the 
+                        career I had worked so hard to achieve was no longer something I wished to continue.
+                    </p>
+                </section>
+                <section>
+                    <h2>A New career</h2>
+                    <p>
+                        In the September of 2018 the student veterinary nurse of the practice was struggling with her studies of drug calculations and asked if I could write her some 
+                        questions on the various topics. Having dabbled in web coding previously with HTML and CSS I offered to write a simple program to generate questions and answers 
+                        for her in Python. At that point I became hooked on coding and kept improving the code I had written with every new piece of knowledge I coud find. Moving from 
+                        procedural to functional and eventually object orientated approaches.
+                    </p>
+                    <p>
+                            Having made the decision to leave veterinary practice for good it seemed that I had found a new direction in coding and part of me wonders if finding this has 
+                            lead to the decision being made. Whilst continuing to work out my notice as a vet I signed up to the Harvard University distance learning introduction to computer 
+                            science to try to fill in the gaps in my knowledge and skills from those that I had developed myself. Discovering not only the joy of coding something that would 
+                            do exactly what I expected it to do but also learning of the frustration when I was not quite close enough for the computer to carry out the required 
+                            processes and deciphering error message after error message. 
+                    </p>
+                    <p>
+                            The culmination of my Harvard course was the Veterinary Calculations Practice which is in fact a web based version of the coding project which hooked me 
+                            to coding in the first place. Since completing the course I am looking for my first development position and as of July 2019 I am currently unemployed whilst the 
+                            job search continues.
+                    </p>
+                </section>
+            </article>
+        </main>
+        <aside class="flexbox3side">
+            <img src="./images/harvard_crest.png" alt="Harvard University Crest"><figcaption>Harvard University, Cambridge, USA</figcaption>
+            <img src="./images/harperadams_crest.png" alt="Harper Adams Crest"><figcaption>Harper Adams University, Newport, UK</figcaption>
+        </aside>
     </div>
-    <div class="flexbox60">
-    <p class="frame2"><img src="./images/david.jpg"></p>
-        <div class="project">
-        <div class="flexbox50">
-                <p class="subtitle">What I can offer</p>     
-                    <ul class="melist">
-                        <li>Determination</li>
-                        <li>Drive</li>
-                        <li>Real world experience</li>
-                        <li>Quick to learn</li>
-                        <li>Cool under pressure</li>
-            </div>
-            <div class="flexbox50">
-            <p class="subtitle">What I'm looking for</p>
-                    <ul class="melist">
-                        <li>Further development of my coding skills</li>
-                        <li>The feeling of being valued member of a team</li>
-                        <li>A chance to produce high quality engaging content</li>
-                        <li>A stimulating and challenging experience</li>
-                        <li>Forward thinking employer</li>
-            </div>
-            <p class="subtitle">Who am I?</p>
-            <p class="textbox">
-                Born: South East London in 1984.
-                <br>
-                <br>
-                Early Education: I left school at 16 to attend a WestMinster KingsWay College in London to allow myself the opportunity to take 4 A-Levels. 4 As (The 
-                highest grades available at the time) in Math. Physics, Chemistry and Biology followed in 2002.
-                <br>
-                <br>
-                First Career: Given my love of animals I decided to persue a career as a vet. I was told at the time that as I went to an inner London college my chances 
-                of achieving this were low, but never having been one to take being told something can't be done at face value I decided that taking a year out to 
-                confirm my grades and gain some further experience would lean things in my favour. This proved to be the case and the following year (Sept. 2003) 
-                I started my studies at the Royal Veterinary College (RVC).
-            </p>
-            <p class="subtitle">University and work until now</p>
-            <p class="textbox">
-                During my studies at the RVC I spent 12 months at Imperial College on an intercalated BSc course achieving a BSc in Medical Sciences with Molecular
-                Biology and Cellular Biology with a 2:1 grade with honours. Returning to the RVC I completed my veterinary studies in 2009 and graduated with a 
-                Bachelor of Veterinary Medicine. I preceeded from there to work as a first opinion veterinarian for 10 years.
-                <br>
-                <br>
-                After 7 years of practice and feeling dissatisfied with my career I under took a post graduate certificate in Small Animal Surgery gaining the certificate in 2016. 
-                Despite this and many other attempts at increasing my work satisfaction I continued to be unhappy at work and eventually in December of 2018 I decided that the 
-                career I had worked so hard to achieve was no longer something I wished to continue.
-            </p>
-            <p class="subtitle">A New career</p>
-            <p class="textbox">
-                In the September of 2018 the student veterinary nurse of the practice was struggling with her studies of drug calculations and asked if I could write her some 
-                questions on the various topics. Having dabbled in web coding previously with HTML and CSS I offered to write a simple program to generate questions and answers 
-                for her in Python. At that point I became hooked on coding and kept improving the code I had written with every new piece of knowledge I coud find. Moving from 
-                procedural to functional and eventually object orientated approaches.
-                <br>
-                <br>
-                Having made the decision to leave veterinary practice for good it seemed that I had found a new direction in coding and part of me wonders if finding this has 
-                lead to the decision being made. Whilst continuing to work out my notice as a vet I signed up to the Harvard University distance learning introduction to computer 
-                science to try to fill in the gaps in my knowledge and skills from those that I had developed myself. Discovering not only the joy of coding something that would 
-                do exactly what I expected it to do but also learning of the frustration when I was not quite close enough for the computer to carry out the required 
-                processes and deciphering error message after error message. 
-                <br>
-                <br>
-                The culmination of my Harvard course was the Veterinary Calculations Practice which is in fact a web based version of the coding project which hooked me 
-                to coding in the first place. Since completing the course I am looking for my first development position and as of July 2019 I am currently unemployed whilst the 
-                job search continues.
-            </p>
-        </div>
-    </div>
-    <div class="flexbox20 gallery">
-    <p class="frame"><img src="./images/harvard_crest.png" alt="Harvard University Crest"><br>Harvard University, Cambridge, USA</p>
-    <p class="frame"><img src="./images/harperadams_crest.png" alt="Harper Adams Crest"><br>Harper Adams University, Newport, UK</p>
-    </div>
-<div>
 </body>
+<footer>
+		All project code can be found at: <a id="gitlink" href="https://github.com/RVJonah">https://github.com/RVJonah</a>
+		<img src="./images/php_logo.gif">
+</footer>
 </html>
--- index.php
+++ index.php
@@ -2,96 +2,100 @@
 <html>
 <header>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="./css/index.css">
+	<link rel="stylesheet" type="text/css" href="./css/base.css">
 <title>
-David Barker's portfolio homepage
+	David Barker's portfolio homepage
 </title>
 </header>
-
 <body>
-	<p class="title"><span>Welcome to David Barker's portfolio</span><br></p>
-	<div class="flexcontainer">
-	<div class="flexbox3side gallery">
-		<p class="frame"><img src="./images/python_small.png" alt="Python logo"></p>
-		<p class="frame"><img src="./images/php_logo.gif" alt="PHP logo"></p>
-		<p class="frame"><img src="./images/css3_logo.jpg" alt="CSS3 logo"></p>
-		<p class="frame"><img src="./images/html5_logo.png" alt="HTML5 logo"></p>
-		<p class="frame"><img src="./images/javascript_logo.png" alt="Javascript logo"></p>
-		<p class="frame"><img src="./images/mysql_logo.png" alt="MySQL logo"></p>
-	</div>
-	<div class="flexbox3middle">
-		<div class="project">
-			<p class="subtitle"> Deployed projects with full functionality</p>
-			<a href="https://vet-calc.herokuapp.com/" class="link"><img class=logo src="./images/vet_logo.jpg"><br>Vet Calculation App</a><br>
-			<p class="intro">My final project from the Harvard University Computer Science Course.</p>
-			<ul class="list">
-				<li>Python server running PostgresSQL via SQLAlchemy. </li>
-				<li>SQLAlchemy makes database switching easier. </li> 
-				<li>The teaching and quiz pages are run via templating in Jinja2 </li>
-				<li>Full frontend form validation via jQuery</li>
-				<li>Test page using Javascript DOM manipulation and CSS </li>
-				<li>Full stack development</li>
-			</ul>
-			<br><br>
-			<a href="./FFVII/index.html" class="link"><img class=logo src="./images/ffvii_logo.jpg"><br>Final Fantasy VII remake page</a><br>
-			<p class="intro">A simple HTML and CSS project to
-			<br>
-			display responsive design skills</p>
-			<ul class="list">
-				<li>HTML5 imbeded images</li>
-				<li>HTML5 imbeded video</li>
-				<li>HTML5 imbeded sound</li> 
-				<li>CSS</li>
-				<li>Responsive web design</li>
-				<li>Bootstrap for gallery carousel and gallery images</li>
-			</ul>
-			<br>
-		</div>
-	<div class="project">	
-	<p class="subtitle"> Projects under construction</p>
-	<a href="./phpLogin" class="link">PHP User Management System</a><br>
-	<p class="intro">Current Functions</p>
-	<ul class="list">
-	<li>Register</li>
-	<li>Login</li>
-	<li>Logout</li>
-	<li>Update</li>
-	<li>Registration email</li>
-	<li>Email verification</li>
-	<li>Email Password Reset </li>
-	<li>Enforced https support</li>
-	<li>Message Display</li>
-	</ul>
-	<p class="intro">To do list</p>
-	<ul class="list">
-	<li>Admin database manipulation</li>
-	<li>Javascript form validation and jQuery</li>
-	<li>CSS overhaul</li>
-	</ul>
-	</div>
-	<div class="project">
-	<p class="subtitle">Future Projects</p>
-	<ul class="list">
-	<li>CSS Animation</li>
-	<li>Photography Store</li>
-	</ul>
-</div>
-	<p class="project subtitle">
-	<a class="melink" href="./aboutme.php">About me</a>
-	</p>
-	</div>
-	<div class="flexbox3side gallery">
-		<p class="frame"><img src="./images/postgresql_logo.png" alt="PostgreSQL logo"></p>
-		<p class="frame"><img src="./images/bootstrap_logo.png" alt="Bootstrap logo"></p>
-		<p class="frame"><img src="./images/sass_logo.png" alt="Sass logo"></p>
-		<p class="frame"><img src="./images/flask_logo.png" alt="Flask logo"></p>
-		<p class="frame"><img src="./images/jinja_logo.png" alt="Jinja logo"></p>
-		<p class="frame"><img src="./images/sqlalchemy_logo.jpg" alt="SQLAlchemy logo"></p>
+	<div id="content">
+		<h1>Welcome to David Barker's portfolio</></h1>
+		<div class="flexcontainer">
+			<aside class="flexbox3side">
+				<img src="./images/python_small.png" alt="Python logo">
+				<img src="./images/php_logo.gif" alt="PHP logo">
+				<img src="./images/css3_logo.jpg" alt="CSS3 logo">
+				<img src="./images/html5_logo.png" alt="HTML5 logo">
+				<img src="./images/javascript_logo.png" alt="Javascript logo">
+				<img src="./images/mysql_logo.png" alt="MySQL logo">
+			</aside>
+			<main class="flexbox3middle">
+				<article  >
+					<section>
+						<h2> Deployed projects with full functionality</h2>
+						<a href="https://vet-calc.herokuapp.com/" class="link"><img class=logo src="./images/vet_logo.jpg">
+						<figcaption>Vet Calculation App</figcaption></a>
+						<h3>My final project from the Harvard University Computer Science Course</h3>
+						<ul>
+							<li>Server : Python Flask</li>
+							<li>Database: PostgreSQL via SQLAlchemy a Python database ORM</li> 
+							<li>HTML: Templating via Jinja2 and JavaScript DOM manipulation </li>
+							<li>API function: /test route only serves JSON</li>
+							<li>CSS: Responsive web design using CSS Grid</li>
+							<li>Full stack development</li>
+						</ul>
+						<a href="./FFVII/index.html"><img class=logo src="./images/ffvii_logo.jpg">
+						<figcaption>Final Fantasy VII remake page</figcaption></a>
+						<h3>A simple HTML and CSS project to display responsive design skills</h3>
+						<ul>
+							<li>HTML5 imbeded images</li>
+							<li>HTML5 imbeded video</li>
+							<li>HTML5 imbeded sound</li> 
+							<li>CSS Grid with responsive web design</li>
+							<li>Bootstrap for gallery carousel and gallery images</li>
+						</ul>
+					</section>
+					<section>	
+						<h2> Projects under construction</h2>
+						<h3>PHP User Management System</h3>
+						<h4>Current Functions</h4>
+						<ul>
+							<li>Register</li>
+							<li>Login</li>
+							<li>Logout</li>
+							<li>Update</li>
+							<li>Registration email</li>
+							<li>Email verification</li>
+							<li>Email Password Reset </li>
+							<li>Enforced https support</li>
+							<li>Message Display</li>
+						</ul>
+						<h4>To do list</h4>
+						<ul>
+							<li>Admin database manipulation</li>
+							<li>Javascript form validation and jQuery</li>
+							<li>CSS overhaul</li>
+						</ul>
+					</section>
+					<section>
+						<h2>Future Projects</h2>
+						<ul>
+							<li>CSS Animation</li>
+							<li>Photography Store</li>
+						</ul>
+					</section>
+					<section>
+						<h2>
+						<a href="./aboutme.php">About me</a>
+						<h2>
+					</section>
+				</article>
+			</main>
+			<aside class="flexbox3side">
+				<img src="./images/postgresql_logo.png" alt="PostgreSQL logo">
+				<img src="./images/bootstrap_logo.png" alt="Bootstrap logo">
+				<img src="./images/sass_logo.png" alt="Sass logo">
+				<img src="./images/flask_logo.png" alt="Flask logo">
+				<img src="./images/jinja_logo.png" alt="Jinja logo">
+				<img src="./images/sqlalchemy_logo.jpg" alt="SQLAlchemy logo">
+			</aside>
+		</div class="flexcontainer">
 	</div>
 </body>
 <footer>
-	All project code can be found at: <a class="gitlink" href="https://github.com/RVJonah">https://github.com/RVJonah</a>
-	<img src="./images/php_logo.gif">
+		All project code can be found at: 
+		<a id="gitlink" href="https://github.com/RVJonah">https://github.com/RVJonah</a>
+		<img src="./images/php_logo.gif">
 </footer>
 
 </html>
